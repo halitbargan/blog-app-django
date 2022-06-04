60a9ac1427ec61a613ad4a7c43ceabe99260861c
halitbargan <hltbrgn@gmail.com>
2022-06-04 21:47:17 +0300

    user_login completed

--- main/urls.py
+++ main/urls.py
@@ -21,6 +21,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('users/', include('users.urls')),
+    
     
     
     
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
+def user_login(request):
+    form = AuthenticationForm(request, data = request.POST)
+    if form.is_valid():
+        user = form.get_user()
+        if user:
+            
+            login(request,user)
+        return redirect ('home')
+    return render (request, 'user/user_login.html', {'form':form})
